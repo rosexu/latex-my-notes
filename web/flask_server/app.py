06181362771bfa83f953e9485b226a3623343533
Rose <rosexu1996@yahoo.com>
2015-09-12 04:07:13 -0400

    integrated with tesseract ocr but handwriting recognition is not good

---
+++ web/flask_server/app.py
@@ -0,0 +1,56 @@
+import logging
+import os
+from logging import Formatter, FileHandler
+from flask import Flask, request, jsonify
+from ocr import process_image
+
+
+app = Flask(__name__)
+_VERSION = 1
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route('/v{}/ocr'.format(_VERSION), methods=["POST"])
+def ocr():
+    try:
+        url = request.json['image_url']
+        if 'jpg' in url:
+            output = process_image(url)
+            return jsonify({"output": output})
+        else:
+            return jsonify({"error": "only .jpg files, please"})
+    except:
+        return jsonify(
+            {"error": "Did you mean to send: {'image_url': 'some_jpeg_url'}"}
+        )
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    print str(error)
+    return 'ok'
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    print str(error)
+    return 'ok'
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: \
+            %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
